0a99ffa7e3c04da81eb4929e52124af627f45731
npearsonat <134087337+npearsonat@users.noreply.github.com>
2025-07-30 18:58:13 -0700

    Update diabetes_dashboard.py

--- diabetes_dashboard.py
+++ diabetes_dashboard.py
@@ -497,7 +497,7 @@ fig_heatmap = px.imshow(
 fig_heatmap.update_xaxes(
     tickmode='array',
     tickvals=[0, 1],
-    ticktext=['Does Not Have', 'Has']
+    ticktext=['Doesn't Exibit Trait', 'Exhibits Trait']
 )
 
 st.plotly_chart(fig_heatmap, use_container_width=True)
